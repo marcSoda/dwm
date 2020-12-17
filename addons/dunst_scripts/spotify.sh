9c5e98dbf07d938a51dc915b9ccb4cc3dc69ba7e
Marc Soda <m.soda412@gmail.com>
2020-12-17 03:04:50 -0500

    spotify song change notification support

---
+++ addons/dunst_scripts/spotify.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+function send_notification {
+    artist=`playerctl metadata artist`
+    song=`playerctl metadata title`
+    album=`playerctl metadata album`
+    dunstify -r 1702 "$artist" "$song\n$album"
+}
+
+case $1 in
+    play-pause)
+	playerctl play-pause
+	;;
+    next)
+	playerctl next
+	send_notification
+	;;
+    previous)
+	playerctl previous
+	send_notification
+	;;
+esac
