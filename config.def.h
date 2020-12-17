e5213907addbb2321d9889c0f9d18440ba1b0ea7
Marc Soda <m.soda412@gmail.com>
2020-12-17 03:10:15 -0500

    fixed gitignore issue

--- config.def.h
+++ config.def.h
@@ -1,5 +1,5 @@
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
